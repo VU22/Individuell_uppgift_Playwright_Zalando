ca8b317841a21f0b42786b119ef67d42af139771
Viktoria <viktoria.berezinski@gmail.com>
2023-10-15 18:02:46 +0200

    update

--- zalando/VG_test/MyTest.java
+++ zalando/VG_test/MyTest.java
@@ -1,32 +1,7 @@
 package VG_test;
 
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-
-import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page.GetByRoleOptions;
-import com.microsoft.playwright.options.AriaRole;
-
 import testbase.TestBase;
 
 public class MyTest extends TestBase{
-	
- @Test
-	
-	void BrandLink () {
-		
-		String productCardUrl = "https://www.zalando.se/tiger-of-sweden-jeans-shelly-jeans-skinny-fit-stay-tn221n03u-q11.html";
-		page.navigate(productCardUrl);
-	
-	Locator tigerOfSweden = page.getByRole(AriaRole.LINK, new GetByRoleOptions().setName("TIGER OF SWEDEN"));
-	
-	tigerOfSweden.click();
-	
-	assertThat(page).hasURL("https://www.zalando.se/tiger-of-sweden/");
-	
-	 System.out.println("Testet lyckades!");
-	
-	}
 
 }
